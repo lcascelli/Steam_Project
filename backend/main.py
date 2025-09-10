78c28be2784c8ebb7f15940b27e6f64c515f977f
lcascelli <liamcascelli@yahoo.com>
2025-09-10 15:07:23 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from routes import predict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Steam Insights ML API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Steam Insights ML API"}
 
 app.include_router(predict.router)
