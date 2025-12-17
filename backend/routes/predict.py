f75b1beed3aac6a8ae850d44dbe5721f38609efe
lcascelli <liamcascelli@yahoo.com>
2025-12-16 16:34:33 -0800

    updating the Access-Control-Allow-Origin function in the CORS preflight for predict.py (ML Model API)

--- backend/routes/predict.py
+++ backend/routes/predict.py
@@ -37,7 +37,7 @@ async def options_predict():
     return JSONResponse(
         content = {"message": "CORS preflight"},
         headers = {
-            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Origin": "https://steam-indie-game-insights.netlify.app",
             "Access-Control-Allow-Methods": "POST, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type",
         }
