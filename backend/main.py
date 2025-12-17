7207f25f22cb88763757efc0d6b9843c425135b4
lcascelli <liamcascelli@yahoo.com>
2025-12-16 17:19:59 -0800

    removing the local access point

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Steam Insights ML API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://steam-indie-game-insights.netlify.app",
-                   "http://localhost:3000"],
+    allow_origins="https://steam-indie-game-insights.netlify.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
